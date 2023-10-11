c45fe04df70b2e70c69a9103b6a7a93dc9df9c36
sharan kumar <sharanperla1234@gmail.com>
2023-10-11 21:21:48 +0530

    add header setups

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Signin from './pages/Signin'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Signup from './pages/Signup'
+import Header from './components/Header'
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/sign-in' element={<Signin/>} />
